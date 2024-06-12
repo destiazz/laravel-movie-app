83bfda870bc28ebd023462c4761e96ce263dd571
destiazz <destiaisnainie@gmail.com>
2024-06-12 11:18:01 +0700

    praktikum13

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+
 use App\Http\Controllers\MovieController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,8 +14,19 @@ Route::get('/', function () {
 
 Route::get('/products', [ProductController::class, 'index']);
 
-Route::resource('/movies', MovieController::class);
 
-Route::resource('/genres', GenreController::class);
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('/reviews', ReviewController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::resource('/movies', MovieController::class);
+    Route::resource('/reviews', ReviewController::class);
+    Route::resource('/genres', GenreController::class);
+});
+
+require __DIR__.'/auth.php';
